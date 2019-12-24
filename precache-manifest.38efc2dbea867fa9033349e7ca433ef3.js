a74ca7db2d15f5b9921f9b7393497a51622b3238
John Carbajal <johnxjuanito@gmail.com>
2019-12-23 17:59:35 -0800

    HOPE

---
+++ precache-manifest.38efc2dbea867fa9033349e7ca433ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab09c9d140f1bd6d430622262c29a549",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bd47d88e902599bc8da",
+    "url": "/static/css/main.0d64c5fe.chunk.css"
+  },
+  {
+    "revision": "ff417cfd583f5297b9d0",
+    "url": "/static/js/2.4a3c046b.chunk.js"
+  },
+  {
+    "revision": "8bd47d88e902599bc8da",
+    "url": "/static/js/main.39cb3f45.chunk.js"
+  },
+  {
+    "revision": "767e35795de6c22d9fb1",
+    "url": "/static/js/runtime-main.8bf9c574.js"
+  },
+  {
+    "revision": "b68e36877dfcd48d5d66f6def1f5949c",
+    "url": "/static/media/4k picture of vegas.b68e3687.jpg"
+  }
+]);
